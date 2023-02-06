a2504aaa9592c4bd66aaee4561a8ac87c1bc1708
Taro Ishihara <taro.ishihara.mail@gmail.com>
2023-02-06 15:56:18 -0800

    add firebase integration

--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -1,32 +1,99 @@
-import * as React from 'react';
-import { Link } from 'react-router-dom';
+import * as React from "react";
+import { Link } from "react-router-dom";
 
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
-import Copyright from '../components/layout/Footer';
+import Copyright from "../components/layout/Footer";
 
-
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  GoogleAuthProvider,
+  signInWithPopup,
+} from "firebase/auth";
 
 const theme = createTheme();
 
 export default function SignIn() {
+  const [user, setUser] = React.useState({
+    email: "",
+    password: "",
+  });
+
+  const handleEmail = (event) => {
+    setUser((user) => {
+      return {
+        ...user,
+        email: event.target.value,
+      };
+    });
+  };
+
+  const handlePassword = (event) => {
+    setUser((user) => {
+      return {
+        ...user,
+        password: event.target.value,
+      };
+    });
+  };
+
+  const signInClick = () => {
+    const auth = getAuth();
+    signInWithEmailAndPassword(auth, user.email, user.password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+      });
+  };
+
+  const signInWithGoogleClick = () => {
+    const provider = new GoogleAuthProvider();
+    const auth = getAuth();
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+        // IdP data available using getAdditionalUserInfo(result)
+        // ...
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+      });
+  };
+
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
     console.log({
-      email: data.get('email'),
-      password: data.get('password'),
+      email: data.get("email"),
+      password: data.get("password"),
     });
   };
 
@@ -37,29 +104,35 @@ export default function SignIn() {
         <Box
           sx={{
             marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
           }}
         >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
             Sign in
           </Typography>
-          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{ mt: 1 }}
+          >
             <Button
               type="submit"
               fullWidth
               variant="contained"
-              color='warning'
+              color="warning"
               sx={{ mt: 3, mb: 2 }}
+              onClick={signInWithGoogleClick}
             >
               Continue With Google
             </Button>
             <Typography component="h4" variant="h7">
-                or
+              or
             </Typography>
             <TextField
               margin="normal"
@@ -70,6 +143,7 @@ export default function SignIn() {
               name="email"
               autoComplete="email"
               autoFocus
+              onChange={handleEmail}
             />
             <TextField
               margin="normal"
@@ -80,6 +154,7 @@ export default function SignIn() {
               type="password"
               id="password"
               autoComplete="current-password"
+              onChange={handlePassword}
             />
             <FormControlLabel
               control={<Checkbox value="remember" color="primary" />}
@@ -90,11 +165,11 @@ export default function SignIn() {
               fullWidth
               variant="contained"
               sx={{ mt: 3, mb: 2 }}
+              onClick={signInClick}
             >
               Sign In
             </Button>
-            
-            
+
             <Grid container>
               <Grid item xs>
                 <Link to="/" variant="body2">
@@ -102,21 +177,19 @@ export default function SignIn() {
                 </Link>
               </Grid>
               <Grid item>
-                <Link to="/signup">
-                  {"Don't have an account? Sign Up"}
-                </Link>
+                <Link to="/signup">{"Don't have an account? Sign Up"}</Link>
               </Grid>
             </Grid>
 
-            <Link to='/dashboard' style={{textDecoration: 'none'}}>
-                <Button
+            <Link to="/dashboard" style={{ textDecoration: "none" }}>
+              <Button
                 type="submit"
                 fullWidth
                 variant="outlined"
                 sx={{ mt: 3, mb: 2 }}
-                >
+              >
                 Continue As Guest
-                </Button>
+              </Button>
             </Link>
           </Box>
         </Box>
